dc305c9cfec404998b488ef5d2ec095f4d3cf50b
ChineseLincoln <sodlinken@gmail.com>
2016-06-14 11:31:43 +0800

    增加MediaType.APPLICATION_JSON
    如果不包含MediaType.APPLICATION_JSON，出现异常就没有办法输出json格式的错误提示

--- src/main/java/com/drawthink/iguyuan/api/ImageService.java
+++ src/main/java/com/drawthink/iguyuan/api/ImageService.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.drawthink.iguyuan.config.ConfigVar;
@@ -21,7 +22,7 @@ public class ImageService {
 	HttpServletRequest request;
 
 	@GET
-	@Produces({"image/jpeg","image/gif ","image/png"})
+	@Produces({"image/jpeg","image/gif ","image/png",MediaType.APPLICATION_JSON})
 	public Response getImage(@PathParam("image") String image) {
 	String localPath = request.getSession().getServletContext().getRealPath("");
 	String filePath = ConfigVar.getUploadImageStoreDir(localPath)+File.separator+image;
